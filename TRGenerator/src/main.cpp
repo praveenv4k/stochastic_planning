802aa4a58ee1afd5e582a86f994ea1382e448ca7
PraveenKumar Vasudevan <praveenv4k@gmail.com>
2014-05-04 19:41:57 +0200

    modified to write the state space

--- TRGenerator/src/main.cpp
+++ TRGenerator/src/main.cpp
@@ -6,6 +6,7 @@
 void testContainer(Container<int>&);
 void testDiscretizer(Container<int>&);
 void testCombinator(Container<int>&,Container<int>&);
+void writeStateSpace();
 
 int main(void){
   Container<int> state1;
@@ -14,19 +15,7 @@ int main(void){
   testContainer(state1);
   testDiscretizer(state1);
   testCombinator(state1,state1);
-  std::cout << "Hello world!";
-  boost::shared_ptr<TrajectoryDiscretizer> pTrajDisc(new CircleTrajectoryDiscretizer(0,0.65,0.4,0.2));
-  boost::shared_ptr<Trajectory> pTraj(new Trajectory(18*M_PI/180,pTrajDisc));
-    
-  std::cout << "Trajectory Points" << std::endl;
-  std::vector<Container<double> > poses;
-  int numPoints=20;
-  if(pTraj->getAllPoses(numPoints,poses)){
-    for(int i=0; i<numPoints;i++){
-      Container<double> pose = poses[i];
-      pose.print();
-    }
-  }
+  writeStateSpace();
   return 0;
 }
 
@@ -52,21 +41,56 @@ void testDiscretizer(Container<int>& state){
   step[0]=0.02;
   step[1]=0.02;
   step[2]=0.02;
-  
-  std::fstream stream;
-  stream.open("/home/praveen/Documents/states.txt",std::fstream::out);
-  Discretizer<double> discretizer(min,max,step);  
-  for(size_t i=0;i<discretizer.size();i++){
-    int id = discretizer();
-    Container<double> val = discretizer.getValueAtIndex(id);
-    stream << val << std::endl;
-  }
-  stream.close();
-  //std::cout << discretizer() << std::endl;
+
+  Discretizer<double> discretizer(min,max,step);
+  discretizer();
 }
 
 void testCombinator(Container<int>& state1, Container<int>& state2){
   Combinator<int> combinator(state1);
   Container<int> state3 = combinator(state2);
   state3.print();
+}
+
+void writeStateSpace(){
+  Container<double> min;
+  min.resize(3);
+  min[0]=-0.25;
+  min[1]=0.45;
+  min[2]=0.25;
+  
+  Container<double> max;
+  max.resize(3);
+  max[0]=0.25;
+  max[1]=0.85;
+  max[2]=0.55;
+  
+  Container<double> step;
+  step.resize(3);
+  step[0]=0.02;
+  step[1]=0.02;
+  step[2]=0.02;
+  
+  boost::shared_ptr<TrajectoryDiscretizer> pTrajDisc(new CircleTrajectoryDiscretizer(0,0.65,0.4,0.2));
+  boost::shared_ptr<Trajectory> pTraj(new Trajectory(18*M_PI/180,pTrajDisc));
+    
+  std::cout << "Trajectory Points" << std::endl;
+  std::vector<Container<double> > poses;
+  int numPoints=20;
+  if(pTraj->getAllPoses(numPoints,poses)){
+    
+  }
+  
+  std::fstream stream;
+  stream.open("states.txt",std::fstream::out);
+  Discretizer<double> discretizer(min,max,step);  
+  for(size_t i=0;i<discretizer.size();i++){
+    int id = discretizer();
+    Container<double> val = discretizer.getValueAtIndex(id);
+    for(int i=0; i<numPoints;i++){
+      Container<double> pose = poses[i];
+      stream << val << " " << pose << std::endl;
+    }
+  }
+  stream.close();
 }
